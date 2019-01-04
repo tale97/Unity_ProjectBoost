7d129bde55cfbc05b63018a479a30682282b0ecb
tale97 <ttle@college.harvard.edu>
2019-01-04 16:56:31 -0500

    add oscillator

--- Assets/Scenes/Oscillator.cs
+++ Assets/Scenes/Oscillator.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+
+    [SerializeField] [Range(0,1)] float movementFactor; // 0 not move, 1 move   
+    Vector3 startingPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f; // about 6.28
+        float rawSineWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSineWave / 2f) + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
     }
 }
