18f620f328080d66ab771487a77d76ef79f3bb84
tale97 <ttle@college.harvard.edu>
2019-01-04 16:08:38 -0500

    oscillator script

---
+++ Assets/Scenes/Oscillator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
